29a36cbba8240789a13365af849c3a5cb53f37ae
brockstonking <brockston.king@gmail.com>
2019-11-07 14:58:39 -0700

    added dropdown for mobile veiw without styling

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './NavBar.css';
 import { Link } from 'react-router-dom';
-import throttle from 'lodash.throttle';
 
 class NavBar extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            isMobile: false
+            isMobile: false,
+            showMenuItemsDropdown: false
         }
     };
 
@@ -17,20 +17,46 @@ class NavBar extends Component {
             isMobile: window.innerWidth < 480 ? true : false
         })
     }
+
+    dropdownHideOrDisplay = () => { this.setState({ showMenuItemsDropdown: this.state.showMenuItemsDropdown ? false : true }) }
     
     render(){
         window.addEventListener('resize', this.checkIfMobileView)
+
+        const menuLinkItems = [
+            {
+                title: 'Home',
+                link: '/'
+            },
+            {
+                title: 'New Patients',
+                link: '/newpatients'
+            },
+            {
+                title: 'Our Team',
+                link: '/aboutus'
+            },
+            {
+                title: 'Contact Us',
+                link: '/contactus'
+            }
+        ]
+
         let menuItems = !this.state.isMobile
         ? <div className='navBarLinkContainer'>
             <Link className='navBarLink navBarHomePage' to='/'><p className='innerNavBarLinkText'>Home</p></Link>
             <Link className='navBarLink navBarNewPatient' to='/newpatients'><p className='innerNavBarLinkText'>New Patients</p></Link>
             <Link className='navBarLink navBarAboutUs' to='/aboutus'><p className='innerNavBarLinkText'>Our Team</p></Link>
-            <Link className='navBarLink navBarContactUs' to='contactus'><p className='innerNavBarLinkText'>Contact Us</p></Link>
+            <Link className='navBarLink navBarContactUs' to='/contactus'><p className='innerNavBarLinkText'>Contact Us</p></Link>
         </div>
         : <div className='navBarLinkContainerMobile'>
-            <img className='menuIcon' alt='' src='' />
-            <div className='menuItemsDropdown'>
-                I have menu items!
+            <img className='menuIcon' src='https://icon-library.net/images/menu-icon-white-png/menu-icon-white-png-27.jpg' alt='' onClick={() => this.dropdownHideOrDisplay()}/>
+            <div className={ this.state.showMenuItemsDropdown ? 'menuItemsDropdown' : 'menuItemsDropdownHidden' }>
+                {menuLinkItems.map( e => {
+                    return <div>
+                        <Link className={`dropdownMenuItem dropdown${e.title.replace(' ', '')}`} to={ e.link } onClick={() => this.dropdownHideOrDisplay()}>{e.title}</Link>
+                    </div>
+                })}
             </div>
         </div>
         return (
