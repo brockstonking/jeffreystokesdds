296cd4422d7e68956eeeca4ef31aa50f045bacd5
brockstonking <brockston.king@gmail.com>
2019-11-07 13:36:47 -0700

    working

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -20,17 +20,25 @@ class NavBar extends Component {
     
     render(){
         window.addEventListener('resize', this.checkIfMobileView)
+        let menuItems = !this.state.isMobile
+        ? <div className='navBarLinkContainer'>
+            <Link className='navBarLink navBarHomePage' to='/'><p className='innerNavBarLinkText'>Home</p></Link>
+            <Link className='navBarLink navBarNewPatient' to='/newpatients'><p className='innerNavBarLinkText'>New Patients</p></Link>
+            <Link className='navBarLink navBarAboutUs' to='/aboutus'><p className='innerNavBarLinkText'>Our Team</p></Link>
+            <Link className='navBarLink navBarContactUs' to='contactus'><p className='innerNavBarLinkText'>Contact Us</p></Link>
+        </div>
+        : <div className='navBarLinkContainerMobile'>
+            <img className='menuIcon' alt='' src='' />
+            <div className='menuItemsDropdown'>
+                I have menu items!
+            </div>
+        </div>
         return (
             <div className='navBarParent'>
                 <div className='logoHolder'>
                     <p className='logoText J'>J</p><p className='logoText S'>S</p>
                 </div>
-                <div className={!this.state.isMobile ? 'navBarLinkContainer' : 'navBarLinkContainerMobile'}>
-                    <Link className='navBarLink navBarHomePage' to='/'><p className='innerNavBarLinkText'>Home</p></Link>
-                    <Link className='navBarLink navBarNewPatient' to='/newpatients'><p className='innerNavBarLinkText'>New Patients</p></Link>
-                    <Link className='navBarLink navBarAboutUs' to='/aboutus'><p className='innerNavBarLinkText'>Our Team</p></Link>
-                    <Link className='navBarLink navBarContactUs' to='contactus'><p className='innerNavBarLinkText'>Contact Us</p></Link>
-                </div>
+                { menuItems }
             </div>
         )
     };
