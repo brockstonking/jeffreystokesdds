cd27e1f8a1be7e70bb67543d4a8c4477d76fc997
Brockston King <brockstonking@Brockstons-MacBook-Pro-2.local>
2019-11-08 09:03:36 -0700

    working

--- src/components/pages/homepage/HomePage.js
+++ src/components/pages/homepage/HomePage.js
@@ -5,13 +5,17 @@ import Testimonials from './testimonials/Testimonials';
 class HomePage extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            isMobile: window.innerWidth < 480 ? true : false,
+        }
     };
 
     render(){
         return (
             <div>
                 <div className='homePageMainPhotoParent'>
-                    <img className='homePageMainPhoto' src='https://i.ibb.co/BzwHM0H/lesly-juarez-1-Ah-GNGKuh-R0-unsplash.jpg' alt='' />
+                    <img className='homePageMainPhoto' src={this.state.isMobile ? '' : 'https://i.ibb.co/BzwHM0H/lesly-juarez-1-Ah-GNGKuh-R0-unsplash.jpg'} alt='' />
                 </div>
                 <div className='titleAndSubTitleParent'>
                     <h1 className='jeffreyStokesDDSTitle'>Jeffrey Stokes, DDS</h1>
