65a900d117e78a7cf776958ca9184d0ea52fc83e
brockstonking <brockston.king@gmail.com>
2019-11-08 11:29:32 -0700

    working

--- src/components/pages/homepage/HomePage.js
+++ src/components/pages/homepage/HomePage.js
@@ -8,7 +8,6 @@ class HomePage extends Component {
         super(props);
 
         this.state = {
-<<<<<<< HEAD
             isMobile: window.innerWidth < 480 ? true : false
         }
     }
@@ -18,22 +17,12 @@ class HomePage extends Component {
             isMobile: window.innerWidth < 480 ? true : false
         })
     }
-=======
-            isMobile: window.innerWidth < 480 ? true : false,
-        }
-    };
->>>>>>> cd27e1f8a1be7e70bb67543d4a8c4477d76fc997
-
     render(){
         window.addEventListener('resize', this.checkIfMobileView);
         return (
             <div>
                 <div className='homePageMainPhotoParent'>
-<<<<<<< HEAD
                     <img className='homePageMainPhoto' src='https://i.ibb.co/DC1Tcg5/smile-2.jpg' alt='' />
-=======
-                    <img className='homePageMainPhoto' src={this.state.isMobile ? '' : 'https://i.ibb.co/BzwHM0H/lesly-juarez-1-Ah-GNGKuh-R0-unsplash.jpg'} alt='' />
->>>>>>> cd27e1f8a1be7e70bb67543d4a8c4477d76fc997
                 </div>
                 <div className='titleAndSubTitleParent'>
                     <h1 className='jeffreyStokesDDSTitle'>Jeffrey Stokes, DDS</h1>
