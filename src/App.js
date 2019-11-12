753c5abf390db20084340172fb0c930384463781
brockstonking <brockston.king@gmail.com>
2019-11-12 13:30:10 -0700

    removed favicon.ico and basename (router)

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   render(){
     window.addEventListener('resize', this.checkIfMobileView);
     return(
-      <Router basename={ process.env.PUBLIC_URL }>
+      <Router>
         <div className='App'>
           <NavBar mobile={this.state.isMobile} />
           { routes }
