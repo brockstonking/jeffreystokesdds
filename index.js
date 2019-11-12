c4061a600eb0123bdeecb7e538495c0a3c3d4cd0
brockstonking <brockston.king@gmail.com>
2019-11-12 07:39:23 -0700

    changed server file

--- index.js
+++ index.js
@@ -55,13 +55,6 @@ const port = PORT || 4005
 //   })
 // });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.listen(port, server_host, () => {
     console.log(`Listening on port ${ port }`);
