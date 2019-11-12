eb11043f6d87d707fd579254841c58fc4c309001
brockstonking <brockston.king@gmail.com>
2019-11-12 14:52:10 -0700

    was not defining nodemailer in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser'); 
 const cors = require('cors');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
