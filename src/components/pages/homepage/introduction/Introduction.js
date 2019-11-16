d4c9b11569f39e5d9f918129e52f2540d0031ec0
Brockston King <brockstonking@205-185-96-227.directcom.com>
2019-11-15 23:53:48 -0700

    working on change current route

--- src/components/pages/homepage/introduction/Introduction.js
+++ src/components/pages/homepage/introduction/Introduction.js
@@ -11,7 +11,7 @@ class Introduction extends Component {
         return (
             <div className='introductionParent'>
                 <h1 className='introductionTitle'>Dentistry Done Right</h1>
-                <p className={this.props.mobile ? 'introductionParagraphMobile' : 'introductionParagraph'}>Dr. Jeffrey Stokes has been practicing general dentistry in the Henderson, NV area for over 25 years. His seasoned experience gives him unique skills to not only help you look good, but also get your oral health back to where it needs to be. Offering general, cosmetic and implant services and much, much more, Dr. Stokes is a dentist for the whole family. To schedule an appointment, calls us at <a href='tel:7025664133' className='homepageIntroductionPhoneLink'>(702) 566-4133</a> or <Link to='/contactus' onClick={() => this.changeCurrentRoute('/contactus')} className='homepageIntroductionOnlineBookingLink'>book it online</Link> today!</p>
+                <p className={this.props.mobile ? 'introductionParagraphMobile' : 'introductionParagraph'}>Dr. Jeffrey Stokes has been practicing general dentistry in the Henderson, NV area for over 25 years. His seasoned experience gives him unique skills to not only help you look good, but also get your oral health back to where it needs to be. Offering general, cosmetic and implant services and much, much more, Dr. Stokes is a dentist for the whole family. To schedule an appointment, calls us at <a href='tel:7025664133' className='homepageIntroductionPhoneLink'>(702) 566-4133</a> or <Link to='/contactus' className='homepageIntroductionOnlineBookingLink'>book it online</Link> today!</p>
             </div>
         )
     }
